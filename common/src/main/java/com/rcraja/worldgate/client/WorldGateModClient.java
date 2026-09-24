88009e6a9646bd673fbf895e6691561a27c691d1
imrcraja <indoadvance3@gmail.com>
2026-09-24 18:59:26 +0530

    Fix LAN play, social profile UI, player menu preview and tab identity

--- common/src/main/java/com/rcraja/worldgate/client/WorldGateModClient.java
+++ common/src/main/java/com/rcraja/worldgate/client/WorldGateModClient.java
@@ -95,6 +95,9 @@ public class WorldGateModClient implements ClientModInitializer {
             );
 
             if (connected) {
+                String displayName = Minecraft.getInstance().getUser().getName();
+                FRIEND_MANAGER.setOnline(displayName);
+
                 WorldGateMod.LOGGER.info(
                         "WorldGate Firebase session ready (uid={})",
                         SESSION.uid()
--- v26_1_2/src/main/java/com/rcraja/worldgate/mixin/TitleScreenMixin.java
+++ v26_1_2/src/main/java/com/rcraja/worldgate/mixin/TitleScreenMixin.java
@@ -3,6 +3,7 @@ package com.rcraja.worldgate.mixin;
 import com.rcraja.worldgate.client.gui.WorldGateScreen;
 
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.PlayerSkinWidget;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 
@@ -44,6 +45,28 @@ public abstract class TitleScreenMixin extends Screen {
             y = Math.max(8, modsButton.getY() - 48);
         } else {
             y = Math.max(8, this.height / 2 + 12);
+        }
+
+        if (this.minecraft != null) {
+            int previewSize = this.width >= 760 ? 150 : 110;
+            int previewX = this.width / 2 + 125;
+            if (previewX + previewSize > this.width - 12) {
+                previewX = this.width / 2 - 125 - previewSize;
+            }
+            if (previewX >= 8) {
+                PlayerSkinWidget playerWidget = new PlayerSkinWidget(
+                        previewSize,
+                        previewSize + 34,
+                        this.minecraft.getEntityModels(),
+                        () -> this.minecraft.getSkinManager()
+                                .getInsecureSkin(this.minecraft.getGameProfile())
+                );
+                playerWidget.setPosition(
+                        previewX,
+                        Math.max(18, this.height / 2 - previewSize / 2 - 10)
+                );
+                this.addRenderableWidget(playerWidget);
+            }
         }
 
         this.addRenderableWidget(
