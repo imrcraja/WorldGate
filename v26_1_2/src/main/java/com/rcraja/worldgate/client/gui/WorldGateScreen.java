7a61b2f3db5aa7732109f4f79d05375b7758d900
imrcraja <indoadvance3@gmail.com>
2026-09-25 22:56:15 +0530

    fix: normalize room code sanitizer newlines

--- v26_1_2/src/main/java/com/rcraja/worldgate/client/gui/WorldGateScreen.java
+++ v26_1_2/src/main/java/com/rcraja/worldgate/client/gui/WorldGateScreen.java
@@ -83,7 +83,10 @@ public class WorldGateScreen extends Screen {
                 Component.translatable("worldgate.roomcode.hint")
         );
         roomCodeBox.setMaxLength(10);
-        roomCodeBox.setValueResponder(value -> {\n            String digits = value.replaceAll("\\D", "");\n            if (!digits.equals(value)) roomCodeBox.setValue(digits);\n        });
+        roomCodeBox.setValueResponder(value -> {
+            String digits = value.replaceAll("\\D", "");
+            if (!digits.equals(value)) roomCodeBox.setValue(digits);
+        });
         roomCodeBox.setHint(Component.literal("Room Code"));
         addRenderableWidget(roomCodeBox);
 
