899de3e85093fb81848dd06f407c788b927d6a21
imrcraja <indoadvance3@gmail.com>
2026-09-25 22:49:16 +0530

    fix: use supported room code input sanitizer

--- v26_1_2/src/main/java/com/rcraja/worldgate/client/gui/WorldGateScreen.java
+++ v26_1_2/src/main/java/com/rcraja/worldgate/client/gui/WorldGateScreen.java
@@ -83,7 +83,7 @@ public class WorldGateScreen extends Screen {
                 Component.translatable("worldgate.roomcode.hint")
         );
         roomCodeBox.setMaxLength(10);
-        roomCodeBox.setFilter(value -> value.matches("\\d{0,10}"));
+        roomCodeBox.setValueResponder(value -> {\n            String digits = value.replaceAll("\\D", "");\n            if (!digits.equals(value)) roomCodeBox.setValue(digits);\n        });
         roomCodeBox.setHint(Component.literal("Room Code"));
         addRenderableWidget(roomCodeBox);
 
